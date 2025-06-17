e6af9a69c149c493ffe7efdaa4affdad618c09d0
sabts <sabrinataraschi@gmail.com>
2025-06-17 11:52:03 +0200

    .

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -10,37 +10,38 @@ produtsController.getAllProduct = async (req, res) => {
   }
 };
 
-produtsController.getUserById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const userFound = await ProductModel.findById(id);
+// produtsController.getUserById = async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const userFound = await ProductModel.findById(id);
 
-    if (!userFound) {
-      res.status(404).send({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).send({ message: "Error gettin user Id" + error });
-  }
-};
+//     if (!userFound) {
+//       res.status(404).send({ message: "User not found" });
+//     }
+//   } catch (error) {
+//     res.status(500).send({ message: "Error gettin user Id" + error });
+//   }
+// };
 
-produtsController.createUser = async (req, res) => {
-  const newUser = new ProductModel({ ...req.body });
-  try {
-    await newUser.save();
-    const allUser = await UserModel.find();
-    res.status(200).send(allUser);
-  } catch (error) {
-    res.status(500).send({ message: "Error creating an user" + error });
-  }
-};
+// produtsController.createUser = async (req, res) => {
+//   const newUser = new ProductModel({ ...req.body });
+//   try {
+//     await newUser.save();
+//     const allUser = await UserModel.find();
+//     res.status(200).send(allUser);
+//   } catch (error) {
+//     res.status(500).send({ message: "Error creating an user" + error });
+//   }
+// };
 
 produtsController.updateProductStock = async (req, res) => {
   const { id } = req.params;
   try {
     await ProductModel.updateOne({ _id: id }, { $set: { ...req.body } });
     const allProduct = await ProductModel.find();
-    console.log(allProduct);
-    // se resta la cantidad cuando se da click al comprar
+    //primero que me lea todo el archivo (✅)
+    // se resta la cantidad cuando se da click al 'comprar'
+    //let quantity = 10;
     // await ProductModel.updateOne({ _id: id }, {$set: {...stock: --quantity}})
     // que pasa si la cantidad de producto no es suficiente?
     // if (quantity > stock) res.(404).send({ message: "No hay suficiente stock" + error });
